b93dc5e09d8b5ae44e1482679526d4a273aa4b26
jbedia <joaquin.bedia@gmail.com>
2014-05-21 11:20:19 +0200

    New version returning 1-row data.frame

--- dictionaryLookup.R
+++ dictionaryLookup.R
@@ -1,14 +1,24 @@
-# Description: Finds the standard variable in the dictionary and returns the standardName
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Performs conversion between non-standard (dataset) and standard (vocabulary) variables
+#' 
+#' Finds the standard variable in the vocabulary matching the corresponding row
+#'  in the dictionary, containing all the necessary information for variable
+#'  transformation. Sub-routine of \code{loadGridDataset}
+#'  
+#'  @param dictionary A valid URL to the .dic file. Argument passed by the loading function
+#'  @param var Character string indicating the variable requested. Argument passed by the loading function
+#'  @return A data frame with one row, whose columns correspond to the different fields of
+#'  the dictionary
+#'  @author J. Bedia \email{joaquin.bedia@@gmail.com}
+
 dictionaryLookup <- function(dictionary, var) {
-      dictionary <- tryCatch({read.csv(dictionary)}, error = function(e) stop("Dictionary not found"))
-      dicRow <- grep(paste("^", var, "$", sep = ""), dictionary$identifier)
-      if (length(dicRow) == 0) {
-            stop("Variable requested does not match any identifier in the dictionary")
-      } else {
-            shortName <- as.character(dictionary$short_name[dicRow])
-						standardName <- as.character(dictionary$identifier[dicRow])
-      }
-      return(list("ShortName" = shortName, "StandardName" = standardName))
+    dictionary <- tryCatch({read.csv(dictionary, stringsAsFactors = FALSE)}, error = function(e) stop("Dictionary not found"))
+    dicRow <- grep(paste("^", var, "$", sep = ""), dictionary$identifier)
+    if (length(dicRow) == 0) {
+        stop("Variable requested does not match any identifier in the dictionary")
+    }
+    return(as.data.frame(dictionary[dicRow, ]))
 }
 # End
+
+
+
