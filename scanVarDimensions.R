bb742cb67fe89991bb9a438a5b795f00dd742984
jbedia <joaquin.bedia@gmail.com>
2014-05-21 11:34:32 +0200

    Dimension names retrieved from axis

--- scanVarDimensions.R
+++ scanVarDimensions.R
@@ -14,22 +14,23 @@
 #' coordinate values are truncated to 1D for conciseness in the inventory.
 #' @references \url{https://www.unidata.ucar.edu/software/thredds/current/netcdf-java/v4.0/javadocAll/ucar/nc2/dt/grid/GeoGrid.html}
 #' @author J. Bedia \email{joaquin.bedia@@gmail.com}
+
 scanVarDimensions <- function(grid) {
     gridShape <- grid$getShape()
     gcs <- grid$getCoordinateSystem()
     dim.list <- list()
     length(dim.list) <- length(gridShape)
-    dimNames <- gsub("\\[|]|\\s|;|=|[0-9]" , "", unlist(strsplit(grid$getDimensions()$toString(), split=",")))
-    names(dim.list) <- dimNames
     if (grid$getRunTimeDimensionIndex() >= 0) {
         rtDimIndex <- grid$getRunTimeDimensionIndex() + 1
         axis <- gcs$getRunTimeAxis()
         dim.list[[rtDimIndex]] <- list("Type" = axis$getAxisType()$toString(), "Units" = axis$getUnitsString(), "Values" = axis$getCoordValues())
+        names(dim.list)[rtDimIndex] <- axis$getDimensionsString()
     }
     if (grid$getEnsembleDimensionIndex() >= 0) {
         ensDimIndex <- grid$getEnsembleDimensionIndex() + 1
         axis <- gcs$getEnsembleAxis()
         dim.list[[ensDimIndex]] <- list("Type" = axis$getAxisType()$toString(), "Units" = axis$getUnitsString(), "Values" = axis$getCoordValues())
+        names(dim.list)[ensDimIndex] <- axis$getDimensionsString()
     }
     if (grid$getTimeDimensionIndex() >= 0) {
         tDimIndex <- grid$getTimeDimensionIndex() + 1
@@ -40,6 +41,7 @@ scanVarDimensions <- function(grid) {
             time.agg <- gcs$getTimeAxisForRun(0L)$getTimeResolution()$toString()
         }
         dim.list[[tDimIndex]] <- list("Type" = axis$getAxisType()$toString(), "TimeStep" = time.agg, "Units" = axis$getUnitsString(), "Date_range" = axis$getCalendarDateRange()$toString())
+        names(dim.list)[tDimIndex] <- axis$getDimensionsString()
     }
     if (grid$getZDimensionIndex() >= 0) {
         zDimIndex <- grid$getZDimensionIndex() + 1
@@ -58,6 +60,7 @@ scanVarDimensions <- function(grid) {
             gridLevels <- gridLevels[-noLevelInd]
         }
         dim.list[[zDimIndex]] <- list("Type" = axis$getAxisType()$toString(), "Units" = axis$getUnitsString(), "Values" = gridLevels)
+        names(dim.list)[zDimIndex] <- axis$getDimensionsString()
     }
     if (grid$getXDimensionIndex() >= 0) {
         xDimIndex <- grid$getXDimensionIndex() + 1
@@ -68,6 +71,7 @@ scanVarDimensions <- function(grid) {
             values <- apply(t(matrix(values, ncol = lonAxisShape[1])), 2, min)
         }
         dim.list[[xDimIndex]] <- list("Type" = axis$getAxisType()$toString(), "Units" = axis$getUnitsString(), "Values" = values)
+        names(dim.list)[xDimIndex] <- axis$getDimensionsString()
     }
     if (grid$getYDimensionIndex() >= 0) {
         yDimIndex <- grid$getYDimensionIndex() + 1
@@ -78,6 +82,7 @@ scanVarDimensions <- function(grid) {
             values <- apply(t(matrix(values, ncol = latAxisShape[1])), 1, min)
         }
         dim.list[[yDimIndex]] <- list("Type" = axis$getAxisType()$toString(), "Units" = axis$getUnitsString(), "Values" = values)
+        names(dim.list)[yDimIndex] <- axis$getDimensionsString()
     }
     return(dim.list)
 }
