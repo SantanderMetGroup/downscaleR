885749b55715ff4d0d8169d62a4feb0b3990c441
jbedia <joaquin.bedia@gmail.com>
2014-05-28 13:07:45 +0200

    Major modification

--- dictionaryTransform.R
+++ dictionaryTransform.R
@@ -1,17 +1,24 @@
-# Description: Performs time bound definitions and variable transformation according to dictionary specifications
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-dictionaryTransform <- function(Data, dic, timePars) {
-	ltb <- as.difftime(dic$lower_time_bound, format = "%H", units = "hours")
-	utb <- as.difftime(dic$upper_time_bound, format = "%H", units = "hours")
-	
-      if (utb - ltb != 0) {
-		dateSliceStart <- timePars$dateSlice - ltb
-		dateSliceEnd <- timePars$dateSlice + utb
-	} else {
-		dateSliceStart <- timePars$dateSlice
-		dateSliceEnd <- dateSliceStart
-	}
-	Data <- Data * dic$scale + dic$offset
-	return(list("dateSliceStart" = dateSliceStart, "dateSliceEnd" = dateSliceEnd, "Data" = Data))
+#' Performs variable transformation
+#' 
+#' Performs variable transformation according to dictionary specifications.
+#' Sub-routine of \code{loadGridDataset}.
+#' 
+#' @param dic Dictionary line for the variable, as returned by \code{dictionaryLookup}
+#' @param timePars A list of time selection parameters, as returned by \code{getTimeDomain}
+#' @param mdArray A n-dimensional array, as returned by \code{makeSubset}
+#' @return a list with start/end dates and de transformed n dimensional array of data.
+#' @author J. Bedia \email{joaquin.bedia@@gmail.com}
+
+dictionaryTransform <- function(dic, grid, timePars, mdArray) {
+	Data <- mdArray * dic$scale + dic$offset
+	if (dic$deaccum != 0) {
+	    dimName <- paste("^", grid$getCoordinateSystem()$getTimeAxis1D()$getName(), "$", sep = "")
+        dimIndex <- grep(dimName, attr(mdArray, "dimensions"))
+        t.ranges <- c(0, cumsum(sapply(1:length(timePars$tRanges), function(x) timePars$tRanges[[x]]$length())))
+	    margin <- c(1:length(attr(mdArray, "dimensions")))[-dimIndex]
+	    Data <- apply(Data, MARGIN = margin, FUN = deaccum, t.ranges)
+    }
+	return(Data)
 }
 # End
+	 
