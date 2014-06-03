40208335b97b0a608f4bd6d5796e6a498b2baaa0
jbedia <joaquin.bedia@gmail.com>
2014-06-03 13:05:55 +0200

    New version

--- R/loadObservations.R
+++ R/loadObservations.R
@@ -12,14 +12,15 @@
 #' @param season
 #' @param years
 #' @return a list with the following elements:
-             # 'VarName': name of the variable requested (Note that the name can be either standard or not depending on wether a dictionary is being used)
-			 # 'isStandard': logical flag indicating if the variable is standard (dictionary is in use) or not.
-			 # 'Stations': A data frame containing the station codes and their names (locations).
-             # 'LonLatCoords': A 2-D matrix with longitude and latitudes of the stations
-			 # 'Dates': A list with the verification times of each record in the time series. This is represented by a list with two elements:
-					# 'Start': The lower time bound of the verification period
-					# 'End': The upper time bound of the verification period
-			 # 'Data': A matrix with the values of the variable. Dates are ordered by rows and Stations in columns.
+#' \begin{itemize}
+#' \item{VarName}{name of the variable requested (Note that the name can be either standard or not depending on wether a dictionary is being used)}
+#' Stations A data frame containing the station codes and their names (locations).
+#' LonLatCoords: A 2-D matrix with longitude and latitudes of the stations
+#' Dates: A list with the verification times of each record in the time series. This is represented by a list with two elements:
+	# 'Start': The lower time bound of the verification period
+	# 'End': The upper time bound of the verification period
+#' Data A matrix with the values of the variable. Dates are ordered by rows and Stations in columns.
+#' \end{itemize}
 #' @export
 #' @author J. Bedia \email{joaquin.bedia@@gmail.com}
 #' @seealso \code{\link{loadGridDataset}}, \code{\link{dataInventory}}
@@ -34,43 +35,27 @@
 # season = 6:8
 # years = 1990:1995
 
-loadObservations <- function(source.dir, file.format = c("ascii", "netcdf"), var, dictionary = NULL, stationID = NULL, lonLim = NULL, latLim = NULL, season = NULL, years = NULL) {
-      ff <- match.arg(file.format, c("ascii", "netcdf"))
-      if ((!is.null(lonLim) | !is.null(latLim)) & !is.null(stationID)) { 
-            lonLim <- NULL 
-            latLim <- NULL
-            warning("lonLim/latLim arguments ignored as Station Codes have been specified.")
-      }
-      if (!is.null(dictionary)) {
-            dic <- dictionaryLookup(dictionary, var)
-            var <- dic$short_name
-            isStandard <- TRUE
-      } else {
-            dic <- NULL
-            isStandard <- FALSE
-      }
-      if (ff == "ascii") {
-            out <- loadObservations.ASCII(source.dir, var, stationID, lonLim, latLim, season, years)
-      } else if (ff == "netcdf") {
-            out <- loadObservations.NetCDF(source.dir, var, stationID, lonLim, latLim, season, years)
-      }
-	if (!is.null(dictionary)) {
-            trans <- dictionaryTransform(out$Data, dic, out$time)
-            out$Data <- trans$Data
-            dateSliceStart <- trans$dateSliceStart
-            dateSliceEnd <- trans$dateSliceEnd
-	} else {
-		varTimeStep <- difftime(out$time$dateSlice[2], out$time$dateSlice[1])
-	  	dateSliceStart <- out$time$dateSlice
-	  	dateSliceEnd <- out$time$dateSlice + varTimeStep
-  	}
-	dateSliceStart <- as.POSIXlt(dateSliceStart)
-	dateSliceEnd <- as.POSIXlt(dateSliceEnd)
-      out$time <- list("Start" = dateSliceStart, "End" = dateSliceEnd)
-      message(paste("[", Sys.time(), "] Done.", sep = ""))
-      return(out)
+loadObservations <- function(source.dir, file.format = c("ascii", "netcdf"), var, stationID = NULL, lonLim = NULL, latLim = NULL, season = NULL, years = NULL) {
+    file.format <- match.arg(file.format, choices = c("ascii", "netcdf"))
+    if ((!is.null(lonLim) | !is.null(latLim)) & !is.null(stationID)) { 
+        lonLim <- NULL 
+        latLim <- NULL
+        warning("lonLim/latLim arguments ignored as Station Codes have been specified.")
+    }
+    if (file.format == "ascii") {
+        out <- loadObservations.ASCII(source.dir, var, stationID, lonLim, latLim, season, years)
+    } else if (file.format == "netcdf") {
+        out <- loadObservations.NetCDF(source.dir, var, stationID, lonLim, latLim, season, years)
+    }
+    varTimeStep <- difftime(out$time[2], out$time[1])
+    dateSliceStart <- out$time
+    dateSliceEnd <- as.POSIXlt(out$time + varTimeStep)
+    out$time <- list("Start" = dateSliceStart, "End" = dateSliceEnd)
+    message(paste("[", Sys.time(), "] Done.", sep = ""))
+    return(out)
 }
 # End      
+
 
 
 
