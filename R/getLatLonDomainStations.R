b4df4c1c5e512eeadc529dbcb2206f3ab34ef266
jbedia <joaquin.bedia@gmail.com>
2014-06-03 13:03:09 +0200

    R-oxygen header added

--- R/getLatLonDomainStations.R
+++ R/getLatLonDomainStations.R
@@ -1,3 +1,17 @@
+#' Define geolocation of station datasets
+#' 
+#' Given the geolocation arguments, finds the index positions and coordinates of the 
+#' requested data, either single point loactions (finds closest --euclidean-- point) or
+#' data within the given bounding box.
+#' 
+#' @param lonLim Numeric. Definition of geolocation in X 
+#' @param latLim Numeric. Definition of geolocation in Y
+#' @param lons Numeric. All available X coordinates in dataset 
+#' @param lats Numeric. All available Y coordinates in dataset 
+#' @return A list with index positions of the requested data and a 2D matrix
+#' of XY coordinates
+#' @author J. Bedia \email{joaquin.bedia@@gmail.com}
+
 getLatLonDomainStations <- function(lonLim, latLim, lons, lats) {
       if (length(lonLim) > 2 | length(latLim) > 2) {
             stop("Invalid definition of geographical position")
