4f23d8254c42a90b523a71510aa63e95972a7a0b
jbedia <joaquin.bedia@gmail.com>
2014-07-16 10:43:47 +0200

    Error capture on attach in case no internet connection is available

--- R/onAttach.R
+++ R/onAttach.R
@@ -9,15 +9,20 @@
       mess <- paste(pkgname, " version ", ver, " (", builddate,") is loaded", sep = "")
       packageStartupMessage(mess)
       url <- "https://raw.githubusercontent.com/SantanderMetGroup/downscaleR/stable/DESCRIPTION"
-      a <- getURL(url, ssl.verifypeer = FALSE)
-      b <- readLines(textConnection(a))
-      latest.ver <- package_version(gsub("Version: ", "", b[grep("Version", b)]))
-      if (ver < latest.ver) {
-            ver.mess1 <- paste("WARNING: Your current version of ", pkgname, " (v", ver, ") is not up-to-date", sep = "")
-            ver.mess <- paste("Get the latest stable version (", latest.ver, ") using <devtools::install_github('SantanderMetGroup/downscaleR@stable'>)", sep = "")
-            ver.mess
-            packageStartupMessage(ver.mess1)
-            packageStartupMessage(ver.mess)
+      con <- tryCatch(getURL(url, ssl.verifypeer = FALSE), error = function(er) {
+            er <- NULL
+            return(er)
+      })
+      if (!is.null(con)) {
+            a <- getURL(url, ssl.verifypeer = FALSE)
+            b <- readLines(textConnection(a))
+            latest.ver <- package_version(gsub("Version: ", "", b[grep("Version", b)]))
+            if (ver < latest.ver) {
+                  ver.mess1 <- paste("WARNING: Your current version of ", pkgname, " (v", ver, ") is not up-to-date", sep = "")
+                  ver.mess <- paste("Get the latest stable version (", latest.ver, ") using <devtools::install_github('SantanderMetGroup/downscaleR@stable'>)", sep = "")
+                  packageStartupMessage(ver.mess1)
+                  packageStartupMessage(ver.mess)
+            }
       }
 }
 # End
