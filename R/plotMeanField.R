f234ccea07a83554dac054cc8eda94a5913e93c2
jbedia <joaquin.bedia@gmail.com>
2014-07-29 18:43:38 +0200

    New plotting capabilities for multi-fields

--- R/plotMeanField.R
+++ R/plotMeanField.R
@@ -1,11 +1,10 @@
-#' Plot a map of the mean value of a grid dataset
+#' @title Plot a map of the mean value of a grid dataset
 #' 
-#' @description Plot the mean of a loaded grid dataset. The funciton is intended for obtaining
-#' a quick visual reference of the data loaded. In case of multi-member gridded datasets, it displays
-#' the multi-member mean
+#' @description Plot the spatial mean of a gridded variable, or variables in the case of multi-fields.
 #' 
 #' @importFrom fields image.plot
 #' @importFrom fields world
+#' @importFrom graphics layout
 #' 
 #' @param gridData A grid dataset
 #' @return a plot of the mean field with a world map superposed
@@ -16,15 +15,38 @@
 #' @note The function plots a simple temporal mean of the loaded object in the form of
 #' a map. It does not handle other temporal aggregations. In case of multimember grid datasets,
 #' It simply plots the multi-member mean.
-#' 
+
 plotMeanField <- function (gridData) {
       dimNames <- attr(gridData$Data, "dimensions")
       mar <- grep("lon|lat", dimNames)
       if (length(mar) != 2) {
             stop("Not a rectangular spatial domain")
       }
-      aux <- apply(gridData$Data, FUN = mean, MARGIN = mar)
-      image.plot(gridData$xyCoords$x, gridData$xyCoords$y, aux, xlab = "", ylab = "", asp = 1)
-      world(add = TRUE)
-} 
+      if (is.na(match("var", dimNames))) {
+            aux <- apply(gridData$Data, FUN = mean, MARGIN = mar)
+            image.plot(gridData$xyCoords$x, gridData$xyCoords$y, aux, xlab = "", ylab = "", asp = 1)
+            world(add = TRUE)
+      } else {
+            var.dim.index <- grep("var", dimNames, fixed = TRUE)
+            n.vars <- length(gridData$Variable$varName)
+            nrows <- ifelse(sqrt(n.vars) < round(sqrt(n.vars)), ceiling(sqrt(n.vars)), floor(sqrt(n.vars)))
+            def.par <- par(no.readonly = TRUE)
+            par(mfrow = dim(mat))
+            # plot(2,2, ty = "n", axes = FALSE, xlab = "", ylab = "")
+            for (i in 1:n.vars) {
+                  aux <- asub(gridData$Data, idx = i, dims = var.dim.index)
+                  mar <- grep("lon|lat", dimNames[-var.dim.index])
+                  aux <- apply(aux, FUN = mean, MARGIN = mar)
+                  # axes <- ifelse(i == 1, TRUE, FALSE)
+                  image.plot(gridData$xyCoords$x, gridData$xyCoords$y, aux, xlab = "", ylab = "", asp = 1) #, axes = axes)
+                  world(add = TRUE)
+                  mtext(gridData$Variable$varName[i])
+            }
+            par(def.par)
+            
+      }
+}
 # End
+            
+            
+       
