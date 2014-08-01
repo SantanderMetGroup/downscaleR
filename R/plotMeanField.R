b1b4c510efc1f1f6af09b4ec8a1f07e5085726c2
jbedia <joaquin.bedia@gmail.com>
2014-08-01 14:37:35 +0200

    Enhancement in plotMeanField
    Allows multi-member plotting

--- R/plotMeanField.R
+++ R/plotMeanField.R
@@ -8,7 +8,10 @@
 #' @importFrom abind asub
 #' 
 #' @param gridData A grid dataset
-#' @return a plot of the mean field with a world map superposed
+#' @param multi.member Should members be plotted sepparately (TRUE), or just a plot
+#'  of the multi-member mean (FALSE, default)?. Ignored if the dataset has no members.
+#' 
+#' @return a plot of the mean/multi-member/multi-variable field with a world map superposed
 #' @export
 #' @details The function is a wrapper of the \code{\link[fields]{image.plot}} function
 #' in package \pkg{fields}
@@ -17,7 +20,7 @@
 #' a map. It does not handle other temporal aggregations. In case of multimember grid datasets,
 #' It simply plots the multi-member mean.
 
-plotMeanField <- function (gridData) {
+plotMeanField <- function (gridData, multi.member = FALSE) {
       dimNames <- attr(gridData$Data, "dimensions")
       mar <- grep("lon|lat", dimNames)
       if (length(mar) != 2) {
@@ -27,28 +30,12 @@ plotMeanField <- function (gridData) {
             aux <- apply(gridData$Data, FUN = mean, MARGIN = mar)
             image.plot(gridData$xyCoords$x, gridData$xyCoords$y, aux, xlab = "", ylab = "", asp = 1, horizontal = TRUE)
             world(add = TRUE)
-      } else {
-            var.dim.index <- grep("var", dimNames, fixed = TRUE)
-            n.vars <- length(gridData$Variable$varName)
-            nrows <- ifelse(sqrt(n.vars) < round(sqrt(n.vars)), ceiling(sqrt(n.vars)), floor(sqrt(n.vars)))
-            mat <- matrix(1, ncol = ceiling(sqrt(n.vars)), nrow = nrows)
-            def.par <- par(no.readonly = TRUE)
-            par(mfrow = dim(mat))
-            # plot(2,2, ty = "n", axes = FALSE, xlab = "", ylab = "")
-            for (i in 1:n.vars) {
-                  aux <- asub(gridData$Data, idx = i, dims = var.dim.index)
-                  mar <- grep("lon|lat", dimNames[-var.dim.index])
-                  aux <- apply(aux, FUN = mean, MARGIN = mar)
-                  # axes <- ifelse(i == 1, TRUE, FALSE)
-                  image.plot(gridData$xyCoords$x, gridData$xyCoords$y, aux, xlab = "", ylab = "", asp = 1, horizontal = TRUE) #, axes = axes)
-                  world(add = TRUE)
-                  mtext(gridData$Variable$varName[i])
+            if (("member" %in% dimNames) & isTRUE(multi.member)) {
+                  multiPlot(gridData, "member")
             }
-            par(def.par)
-            
+      } else {
+            multiPlot(gridData, "var")
       }
 }
 # End
-            
-            
        
