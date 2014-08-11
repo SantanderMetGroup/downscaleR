10d83c78fd999eff8ad9161d60cba42a3a7c4cc5
jbedia <joaquin.bedia@gmail.com>
2014-08-11 11:07:51 +0200

    Indentation test -does not affect the code-

--- R/biasCorrection.R
+++ R/biasCorrection.R
@@ -5,59 +5,59 @@
 #' 
 
 biasCorrection <- function (obs, pred, sim, method = c("qqmap", "delta", "scaling", "unbiasing", "piani"), pr.threshold = 1) {
-  
-method<-match.arg(method, choices = c("qqmap", "delta", "unbiasing", "piani", "scaling"))
-threshold<-pr.threshold
-
-dimDiff<-NULL
-dimPerm <- 1:length(attr(pred$Data, "dimensions"))
-if (length(setdiff(attr(pred$Data, "dimensions"),attr(obs$Data, "dimensions")))>0){
-  dimDiff<-setdiff(attr(pred$Data, "dimensions"),attr(obs$Data, "dimensions"))
-  for (k in 1:length(dimDiff)) {
-    dimPerm[which(grepl(dimDiff[k],attr(pred$Data, "dimensions")))]<-length(attr(obs$Data, "dimensions"))+k
-  }
-}
-dimPerm[which(dimPerm<=length(dim(obs$Data)))]<-  match(attr(obs$Data, "dimensions"), attr(pred$Data, "dimensions"))
-
-if (length(dimDiff)==0){
-  F <- calibrateProj(obs$Data, aperm(pred$Data,dimPerm), aperm(sim$Data,dimPerm), method = method, varcode = obs$Variable$varName, pr.threshold = threshold)
-  sim$Data<-aperm(F,c(dimPerm[2:length(dim(obs$Data))],dimPerm[1]))
-}else{
-#  commonIndex<-NULL
-# for (n in 1:length(dim(obs$Data))){
-#    commonIndex<-c(commonIndex,as.list(1:dim(obs$Data)[n]))
-#  }
-  P<-aperm(pred$Data,dimPerm)
-  F<-aperm(sim$Data,dimPerm)
-}
-if (length(dimDiff)==1){
-  for (n in 1:dim(sim$Data)[length(dim(obs$Data))+1]){
-    F1 <- calibrateProj(obs$Data, P[,,,n], F[,,,n], method = method, varcode = obs$Variable$varName, pr.threshold = threshold)
-#    sim$Data[,,,n]<-aperm(F1,c(dimPerm[2:length(dim(obs$Data))],dimPerm[1],dimPerm[(length(dim(obs$Data))+1):length(dim(pred$Data))]))
-    sim$Data[,,,n]<-aperm(F1,c(dimPerm[2:length(dim(obs$Data))],dimPerm[1]))
-  }
-}
-if (length(dimDiff)==2){
-  for (n in 1:dim(sim$Data)[length(dim(obs$Data))+1]){
-    for (m in 1:dim(sim$Data)[length(dim(obs$Data))+2]){
-      F1 <- calibrateProj(obs$Data, P[,,,n,m], F[,,,n,m], method = method, varcode = obs$Variable$varName, pr.threshold = threshold)
-      sim$Data[,,,n,m]<-aperm(F1,c(dimPerm[2:length(dim(obs$Data))],dimPerm[1]))
-    }
-  }
-}
-if (length(dimDiff)==3){
-  for (n in 1:dim(sim$Data)[length(dim(obs$Data))+1]){
-    for (m in 1:dim(sim$Data)[length(dim(obs$Data))+2]){
-      for (t in 1:dim(sim$Data)[length(dim(obs$Data))+3]){
-        F1 <- calibrateProj(obs$Data, P[,,,n,m,t], F[,,,n,m,t], method = method, varcode = obs$Variable$varName, pr.threshold = threshold)
-        sim$Data[,,,n,m,t]<-aperm(F1,c(dimPerm[2:length(dim(obs$Data))],dimPerm[1]))
+      
+      method<-match.arg(method, choices = c("qqmap", "delta", "unbiasing", "piani", "scaling"))
+      threshold<-pr.threshold
+      
+      dimDiff<-NULL
+      dimPerm <- 1:length(attr(pred$Data, "dimensions"))
+      if (length(setdiff(attr(pred$Data, "dimensions"),attr(obs$Data, "dimensions")))>0){
+            dimDiff<-setdiff(attr(pred$Data, "dimensions"),attr(obs$Data, "dimensions"))
+            for (k in 1:length(dimDiff)) {
+                  dimPerm[which(grepl(dimDiff[k],attr(pred$Data, "dimensions")))]<-length(attr(obs$Data, "dimensions"))+k
+            }
       }
-    }
-  }
-}
-if (any(grepl(obs$Variable$varName,c("pr","tp","precipitation","precip")))){
-  attr(sim$Data, "threshold") <-  threshold
-}
-attr(sim$Data, "correction") <-  method
-return(sim)
+      dimPerm[which(dimPerm<=length(dim(obs$Data)))]<-  match(attr(obs$Data, "dimensions"), attr(pred$Data, "dimensions"))
+      
+      if (length(dimDiff)==0){
+            F <- calibrateProj(obs$Data, aperm(pred$Data,dimPerm), aperm(sim$Data,dimPerm), method = method, varcode = obs$Variable$varName, pr.threshold = threshold)
+            sim$Data<-aperm(F,c(dimPerm[2:length(dim(obs$Data))],dimPerm[1]))
+      }else{
+            #  commonIndex<-NULL
+            # for (n in 1:length(dim(obs$Data))){
+            #    commonIndex<-c(commonIndex,as.list(1:dim(obs$Data)[n]))
+            #  }
+            P<-aperm(pred$Data,dimPerm)
+            F<-aperm(sim$Data,dimPerm)
+      }
+      if (length(dimDiff)==1){
+            for (n in 1:dim(sim$Data)[length(dim(obs$Data))+1]){
+                  F1 <- calibrateProj(obs$Data, P[,,,n], F[,,,n], method = method, varcode = obs$Variable$varName, pr.threshold = threshold)
+                  #    sim$Data[,,,n]<-aperm(F1,c(dimPerm[2:length(dim(obs$Data))],dimPerm[1],dimPerm[(length(dim(obs$Data))+1):length(dim(pred$Data))]))
+                  sim$Data[,,,n]<-aperm(F1,c(dimPerm[2:length(dim(obs$Data))],dimPerm[1]))
+            }
+      }
+      if (length(dimDiff)==2){
+            for (n in 1:dim(sim$Data)[length(dim(obs$Data))+1]){
+                  for (m in 1:dim(sim$Data)[length(dim(obs$Data))+2]){
+                        F1 <- calibrateProj(obs$Data, P[,,,n,m], F[,,,n,m], method = method, varcode = obs$Variable$varName, pr.threshold = threshold)
+                        sim$Data[,,,n,m]<-aperm(F1,c(dimPerm[2:length(dim(obs$Data))],dimPerm[1]))
+                  }
+            }
+      }
+      if (length(dimDiff)==3){
+            for (n in 1:dim(sim$Data)[length(dim(obs$Data))+1]){
+                  for (m in 1:dim(sim$Data)[length(dim(obs$Data))+2]){
+                        for (t in 1:dim(sim$Data)[length(dim(obs$Data))+3]){
+                              F1 <- calibrateProj(obs$Data, P[,,,n,m,t], F[,,,n,m,t], method = method, varcode = obs$Variable$varName, pr.threshold = threshold)
+                              sim$Data[,,,n,m,t]<-aperm(F1,c(dimPerm[2:length(dim(obs$Data))],dimPerm[1]))
+                        }
+                  }
+            }
+      }
+      if (any(grepl(obs$Variable$varName,c("pr","tp","precipitation","precip")))){
+            attr(sim$Data, "threshold") <-  threshold
+      }
+      attr(sim$Data, "correction") <-  method
+      return(sim)
 }
