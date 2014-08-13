30f3e4285e0b4eda8afe80761db570d9ef351ebf
jbedia <joaquin.bedia@gmail.com>
2014-08-13 11:43:37 +0200

    Minor bug fix and enhancements in interpGridData

--- R/interpGridData.R
+++ R/interpGridData.R
@@ -4,36 +4,36 @@
 #' or nearest-neighbour methods.
 #' 
 #' @importFrom fields interp.surface.grid
+#' @importFrom abind abind
 #' 
-#'  
-#'  @param gridData A grid data object coming from \code{\link{loadGridData}} or the \pkg{ecomsUDG.Raccess} 
-#'  package function \code{\link[ecomsUDG.Raccess]{loadECOMS}}.
-#'  @param new.grid Definition of the new grid, in the form of a list with the x and y components, in thir order.
-#'  Each component consists of a vector of length three with components \emph{from}, \emph{to} and \emph{by},
-#'   in this order, similar as the arguments passed to the \code{\link[base]{seq}} function, giving the 
-#'   westernmost, easternmost and grid cell width in the X axis and, in the same way,
-#'   the southernmost, northernmost and grid cell resolution in the Y axis. See details.
-#'  @param method Method for interpolation. Currently implemented methods are either \code{bilinear},
-#'  for bilinear interpolation, and \code{nearest}, for nearest-neighbor interpolation.
-#'  @return An interpolated object preserving the output structure of the input
-#'   (See e.g. \code{\link{loadGridData}}) for details on the output structure. 
-#'  @details  In case of default definition of either x, y or both grid coordinates, the default grid
-#'  is calculated taking the corners of the current grid and assuming x and y resolutions equal to 
-#'  the default \code{by} argument value in function \code{\link[base]{seq}}: \emph{by = ((to - from)/(length.out - 1))}.
-#'  The bilinear interpolator is a wrapper of the \code{\link[fields]{interp.surface.grid}} function
-#'  in package \pkg{fields}.
-#'  The output has special attributes in the \code{xyCoords} element that indicate that the object
-#'   has been interpolated. These attributes are \code{interpolation}, which indicates the method used and
-#'   \code{resX} and \code{resY}, for the grid-cell resolutions in the X and Y axes respectively.
-#'   It is also possible to pass the interpolator the grid of a previously existing grid dataset using the
-#'   \code{\link{getGrid}} method.
-#'  @note To avoid unnecessary NA values, the function will not extrapolate using a new grid outside the
-#'  current extent of the dataset, returning an error message.
-#'  @family loading.grid
-#'  @author J. Bedia \email{joaquin.bedia@@gmail.com} and S. Herrera
-#'  @export
-#'  @examples \dontrun{
-#' # This is the path to the package built-in NCEP dataset (assumes reading permission)
+#' @param gridData A grid data object coming from \code{\link{loadGridData}} or the \pkg{ecomsUDG.Raccess} 
+#' package function \code{\link[ecomsUDG.Raccess]{loadECOMS}}.
+#' @param new.grid Definition of the new grid, in the form of a list with the x and y components, in thir order.
+#' Each component consists of a vector of length three with components \emph{from}, \emph{to} and \emph{by},
+#'  in this order, similar as the arguments passed to the \code{\link[base]{seq}} function, giving the 
+#'  westernmost, easternmost and grid cell width in the X axis and, in the same way,
+#'  the southernmost, northernmost and grid cell resolution in the Y axis. See details.
+#' @param method Method for interpolation. Currently implemented methods are either \code{bilinear},
+#' for bilinear interpolation, and \code{nearest}, for nearest-neighbor interpolation.
+#' @return An interpolated object preserving the output structure of the input
+#'  (See e.g. \code{\link{loadGridData}}) for details on the output structure. 
+#' @details  In case of default definition of either x, y or both grid coordinates, the default grid
+#' is calculated taking the corners of the current grid and assuming x and y resolutions equal to 
+#' the default \code{by} argument value in function \code{\link[base]{seq}}: \emph{by = ((to - from)/(length.out - 1))}.
+#' The bilinear interpolator is a wrapper of the \code{\link[fields]{interp.surface.grid}} function
+#' in package \pkg{fields}.
+#' The output has special attributes in the \code{xyCoords} element that indicate that the object
+#'  has been interpolated. These attributes are \code{interpolation}, which indicates the method used and
+#'  \code{resX} and \code{resY}, for the grid-cell resolutions in the X and Y axes respectively.
+#'  It is also possible to pass the interpolator the grid of a previously existing grid dataset using the
+#'  \code{\link{getGrid}} method.
+#' @note To avoid unnecessary NA values, the function will not extrapolate using a new grid outside the
+#' current extent of the dataset, returning an error message.
+#' @family loading.grid
+#' @author J. Bedia \email{joaquin.bedia@@gmail.com} and S. Herrera
+#' @export
+#' @examples \dontrun{
+#' # This is the path to the package built-in NCEP dataset (assumes read permission)
 #'  ncep <- file.path(find.package("downscaleR"), "datasets/reanalysis/Iberia_NCEP/Iberia_NCEP.ncml")
 #' # Load air temperature at 1000 mb isobaric pressure level for boreal winter (DJF) 1991-2000
 #' t1000.djf <- loadGridData(ncep, var = "ta@@100000", lonLim = c(-12,10), latLim = c(33,47), season = c(12,1,2), years = 1991:2000)
@@ -47,83 +47,45 @@
 #' str(t1000.djf.05$xyCoords)
 #' }
 
-
 interpGridData <- function(gridData, new.grid = list(x = NULL, y = NULL), method = c("bilinear", "nearest")) {
+      if (is.null(new.grid)) {
+            new.grid <- list(x = NULL, y = NULL)
+      }
+      method <- match.arg(method, choices = c("bilinear", "nearest"))
       x <- gridData$xyCoords$x
       y <- gridData$xyCoords$y
+      names(new.grid) <- c("x", "y")
       # Definition of new grid
       if (is.null(new.grid$x)) {
             new.grid.x <- gridData$xyCoords$x
       } else {
-        if (!exists("resX", where = attributes(new.grid))) {
-              new.grid.x <- do.call("seq", as.list(new.grid$x))
-        } else {
-          if (length(new.grid$x) != 2 | new.grid$x[2] < new.grid$x[1]) {
-            stop("Invalid grid definition in X")
-          }
-          if (new.grid$x[1] < floor(x[1]) | new.grid$x[2] > ceiling(tail(x, 1))) {
-            warning("The new longitudes are outside the data extent")
-          }
-          new.grid.x <- do.call("seq", as.list(c(new.grid$x, attr(new.grid, 'resX'))))
-        }
+            if (!exists("resX", where = attributes(new.grid))) {
+                  new.grid.x <- do.call("seq", as.list(new.grid$x))
+            } else {
+                  if (length(new.grid$x) != 2 | new.grid$x[2] < new.grid$x[1]) {
+                        stop("Invalid grid definition in X")
+                  }
+                  if (new.grid$x[1] < floor(x[1]) | new.grid$x[2] > ceiling(tail(x, 1))) {
+                        warning("The new longitudes are outside the data extent")
+                  }
+                  new.grid.x <- do.call("seq", as.list(c(new.grid$x, attr(new.grid, 'resX'))))
+            }
       }
       if (is.null(new.grid$y)) {
-        new.grid.y <- gridData$xyCoords$y
-      }else{
-        if (!exists("resY", where = attributes(new.grid))) {
-              new.grid.y <- do.call("seq", as.list(new.grid$y))
-        }else{
-          if (length(new.grid$y) != 2 | new.grid$y[2] < new.grid$y[1]) {
-            stop("Invalid grid definition in Y")
-          }
-          if (new.grid$y[1] < floor(y[1]) | new.grid$y[2] > ceiling(tail(y, 1))) {
-            warning("The new latitudes are outside the data extent")
-          }
-          new.grid.y <- do.call("seq", as.list(c(new.grid$y, attr(new.grid, 'resY'))))
-        }
+            new.grid.y <- gridData$xyCoords$y
+      } else {
+            if (!exists("resY", where = attributes(new.grid))) {
+                  new.grid.y <- do.call("seq", as.list(new.grid$y))
+            } else {
+                  if (length(new.grid$y) != 2 | new.grid$y[2] < new.grid$y[1]) {
+                        stop("Invalid grid definition in Y")
+                  }
+                  if (new.grid$y[1] < floor(y[1]) | new.grid$y[2] > ceiling(tail(y, 1))) {
+                        warning("The new latitudes are outside the data extent")
+                  }
+                  new.grid.y <- do.call("seq", as.list(c(new.grid$y, attr(new.grid, 'resY'))))
+            }
       }
-#       if (exists("resX", where = attributes(new.grid))) {
-#         if (is.null(new.grid$x)) {
-#           new.grid.x <- seq(x[1], tail(x, 1))
-#         } else {
-#           if (length(new.grid$x) != 2 | new.grid$x[2] < new.grid$x[1]) {
-#             stop("Invalid grid definition in X")
-#           }
-#           if (new.grid$x[1] < floor(x[1]) & new.grid$x[2] <= ceiling(tail(x, 1))) {
-#             stop("The westernmost corner of the new grid is outside the data extent\n Minimum X accepted value: ", floor(x[1]))
-#           }
-#           if (new.grid$x[2] > ceiling(tail(x, 1)) & new.grid$x[1] >= floor(x[1])) {
-#             stop("The easternmost corner of the new grid is outside the data extent\n Maximum X accepted value: ", ceiling(tail(x, 1)))
-#           }
-#           if (new.grid$x[2] > ceiling(tail(x, 1)) & new.grid$x[1] < floor(x[1])) {
-#             stop("The new grid is outside the data extent\n Accepted X values in the range: [", floor(x[1]), ",", ceiling(tail(x, 1)), "]")
-#           }
-#           new.grid.x <- do.call("seq", as.list(c(new.grid$x, attr(new.grid, 'resX'))))
-#         }
-#       } else {
-#         new.grid.x <- new.grid$x
-#       }
-#       if (exists("resY", where = attributes(new.grid))) {
-#         if (is.null(new.grid$y)) {
-#           new.grid.y <- seq(y[1], tail(y, 1))
-#         } else {
-#           if (length(new.grid$y) != 2 | new.grid$y[2] < new.grid$y[1]) {
-#             stop("Invalid grid definition in Y")
-#           }
-#           if (new.grid$y[1] < floor(y[1]) & new.grid$y[2] <= ceiling(tail(y, 1))) {
-#             stop("The southernmost corner of the new grid is outside the data extent\n Minimum Y accepted value: ", floor(y[1]))
-#           }
-#           if (new.grid$y[2] > ceiling(tail(y, 1)) & new.grid$y[1] >= floor(y[1])) {
-#             stop("The northernmost corner of the new grid is outside the data extent\n Maximum Y accepted value: ", ceiling(tail(y, 1)))
-#           }
-#           if (new.grid$y[2] > ceiling(tail(y, 1)) & new.grid$y[1] < floor(y[1])) {
-#             stop("The new grid is outside the data extent\n Accepted Y values in the range: [", floor(y[1]), ",", ceiling(tail(y, 1)), "]")
-#           }
-#           new.grid.y <- do.call("seq", as.list(c(new.grid$y, attr(new.grid, 'resY'))))
-#         }
-#       } else {
-#         new.grid.y <- new.grid$y
-#       }
       grid.list <- list("x" = new.grid.x, "y" = new.grid.y)
       new.grid.x <- NULL
       new.grid.y <- NULL
