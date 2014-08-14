bf57188dcf9bf447777d43a990131bef03832930
jbedia <joaquin.bedia@gmail.com>
2014-08-14 20:36:11 +0200

    New function for EOF analysis

---
+++ R/prinComp.R
@@ -0,0 +1,186 @@
+#' @title Principal Component Analysis of field/multifield data
+#' @description Performs a Principal Component Analysis of field or multifield data
+#' 
+#' @param gridData A multifield object or a single field as returned by loadGridData
+#' @param n.eofs Number of EOFs to be retained. Default to \code{NULL}, indicating
+#'  that either all EOFs are kept, or that the next argument will be used as criterion
+#'  for its determination. See next argument and details.
+#' @param v.exp Maximum fraction of explained variance, in the range (0,1]. Used to determine the number of EOFs 
+#' to be retained, as an alternative to \code{n.eofs}. Default to \code{NULL}. See details.
+#' @param scaling Method for performing the scaling (and centering) of the input raw data matrix.
+#' Currently accepted choices are \code{"field"} (the default) and \code{"gridbox"}. See details.
+#' 
+#' @return A list of \emph{N + 1} elements for multifields, where \emph{N} is the number of input variables used
+#'  and the last element contains the results of the combined PCA (See details). In case of single fields
+#'  (1 variable only), a list of length 1. For each element of the list, the following 
+#'  objects are returned:
+#'  
+#'  \itemize{
+#'  \item \code{PCs}: A matrix of principal components, arranged in columns by decreasing importance order 
+#'  \item \code{EOFs}: A matrix of EOFs, arranged in columns by decreasing importance order
+#'  }
+#'  The \dQuote{order of importance} is given by the explained variance of each PC, as indicated
+#'  in the attribute \code{"explained_variance"} as a cumulative vector.
+#'  Additional information is returned via the remaining attributes (see Details).
+#' 
+#' @details 
+#' 
+#' \strong{Number of EOFs}
+#' 
+#' \code{n.eofs} and \code{v.exp} are alternative choices for the determination
+#'  of the number of EOFs (hence also the corresponding PCs) to be retained. If both are \code{NULL} (the default)
+#'  , all EOFs will be retained. If both are given a value different from \code{NULL}, 
+#'  the \code{n.eofs} argument will prevail, and \code{v.exp} will be ignored, with a warning.
+#'  Note that when dealing with multifields, the \code{n.eofs} argument will return the same number of EOFs
+#'  for each variable by sepparate and also for the combined field, while \code{v.exp} will select
+#'  a different number, depending on the explained variance in each case. 
+#'  
+#' \strong{Scaling and centering}
+#' 
+#' In order to eliminate the effect of the varying scales of the different climate variables, the input
+#' data matrix is always scaled and centered, and there is no choice to avoid this step. However, the mean
+#' and standard deviation can be either computed for each grid box individually (\code{"gridbox"}) or for all
+#' grid-boxes together (i.e., at the field level, \code{"field"}). The last case is the preferred choice and has
+#' been set as default, returning one single mean and sigma parameter for each variable. If the \code{"gridbox"}
+#' approach is selected, a vector of length \emph{n}, where \emph{n} is the number of grid-cells composing the 
+#' field, is returned for both the mean and sigma parameters (this is equivalent to using the \code{\link{scale}}
+#' function with the input data matrix). 
+#' 
+#' The method used is returned as a global attribute of the returned object (\code{"scaled:method"}), and the 
+#' \emph{mu} and \emph{sigma} parameters are returned as attributes of the corresponding variables
+#'  (\code{"scaled:scale"} and \code{"scaled:center"} respectively).
+#' 
+#' \strong{Combined EOF analysis}
+#' 
+#' When dealing with multifield data, apart from the PCA analysis performed on each variable indivisually,
+#' a combined analysis considering all variables together is done. This is always returned in the last element
+#' of the output list.
+#'
+#' @export
+#' 
+#' @family multifield
+#' @family loading.grid
+#' 
+#' @references
+#' Guti\'{e}rrez, J.M., R. Ancell, A. S. Cofi\~{n}o and C. Sordo (2004). Redes Probabil\'{i}sticas
+#'  y Neuronales en las Ciencias Atmosf\'{e}ricas. MIMAM, Spain. 279 pp.
+#'   \url{http://www.meteo.unican.es/en/books/dataMiningMeteo}
+#'  
+#' @author J. Bedia \email{joaquin.bedia@@gmail.com}
+
+prinComp <- function(gridData, n.eofs = NULL, v.exp = NULL, scaling = c("field", "gridbox")) {
+      if (!is.null(n.eofs) & !is.null(v.exp)) {
+            warning("The 'v.exp' argument was ignored as 'n.eofs' has been indicated")
+      }
+      if (is.null(n.eofs) & is.null(v.exp)) {
+            warning("All possible PCs/EOFs retained: This may result in an unnecessarily large object")
+      }
+      if (!is.null(n.eofs)) {
+            v.exp <- NULL
+            if (n.eofs < 1) {
+                  stop("Invalid number of EOFs selected")
+            }
+      }
+      if (!is.null(v.exp)) {
+            if (!(v.exp > 0 & v.exp <= 1)) {
+                  stop("The explained variance threshold must be in the range (0,1]")
+            }
+      }
+      scaling <- match.arg(scaling, choices = c("field", "gridbox"))
+      if (length(gridData$xyCoords$x) < 2 & length(gridData$xyCoords$y) < 2) {
+            stop("The dataset is not a field encompassing multiple grid-cells")
+      }
+      dimNames <- attr(gridData$Data, "dimensions")
+      indices <- rep(list(bquote()), length(dimNames))
+      # Multi-variable
+      if ("var" %in% dimNames) {
+            var.index <- grep("var", dimNames)
+            n.vars <- dim(gridData$Data)[var.index]
+            var.list <- lapply(1:n.vars, function(x) {
+                  indices[[var.index]] <- x
+                  call <- as.call(c(list(as.name("["), quote(gridData$Data)), indices))
+                  l <- eval(call)
+                  attr(l, "dimensions") <- dimNames[-var.index]
+                  l <- array3Dto2Dmat(l)
+                  return(l)
+            })
+      } else {
+            var.list <- list()
+            var.list[[1]] <- array3Dto2Dmat(gridData$Data)
+      }
+      # Scaling and centering
+      if (scaling == "field") {
+            Xsc.list <- lapply(1:length(var.list), function(x) {
+                  mu <- mean(var.list[[x]], na.rm = TRUE)
+                  sigma <- sd(var.list[[x]], na.rm = TRUE)
+                  Xsc <- (var.list[[x]] - mu) / sigma
+                  attr(Xsc, "scaled:center") <- mu
+                  attr(Xsc, "scaled:scale") <- sigma
+                  return(Xsc)
+            })
+      } else {
+            Xsc.list <- lapply(1:length(var.list), function(x) {
+                  scale(var.list[[x]], center = TRUE, scale = TRUE)
+            })
+      }
+      var.list <- NULL
+      # Combined PCs
+      if (length(Xsc.list) > 1) {
+            message("[", Sys.time(), "] Performing PC analysis on ", length(Xsc.list), " variables plus their combination...")
+            Xsc.list[[length(Xsc.list) + 1]] <- do.call("cbind", Xsc.list)
+      } else {
+            message("[", Sys.time(), "] Performing PC analysis on one variable...")
+      }
+      # PCs
+      pca.list <- lapply(1:length(Xsc.list), function(x) {
+            # Covariance Matrix
+            Cx <- cov(Xsc.list[[x]])
+            # Singular vectors (EOFs) and values
+            sv <- svd(Cx)
+            F <- sv$u
+            # F <- eigen(Cx)$vectors
+            lambda <- sv$d
+            # lambda <- eigen(Cx)$values            
+            sv <- NULL
+            # Explained variance
+            explvar <- cumsum(lambda / sum(lambda))
+            # Number of EOFs to be retained
+            if (!is.null(v.exp)) {
+                  n <- findInterval(v.exp, explvar) + 1
+            } else { 
+                  if (!is.null(n.eofs)) {
+                        n <- n.eofs
+                  } else {
+                        n <- length(explvar)
+                  }
+            }
+            F <- F[ ,1:n]
+            explvar <- explvar[1:n]
+            # PCs
+            PCs <- t(t(F) %*% t(Xsc.list[[x]]))
+            out <- list("PCs" = PCs, "EOFs" = F)
+            attr(out, "scaled:center") <- attr(Xsc.list[[x]], "scaled:center")
+            attr(out, "scaled:scale") <- attr(Xsc.list[[x]], "scaled:scale")
+            attr(out, "explained_variance") <- explvar
+            return(out)
+      })
+      Xsc.list <- NULL
+#       # Recover field
+#       Xhat <- t(F %*% t(PCs))
+#       str(Xhat)
+#       dim(Xsc)
+#       image.plot(Xsc.list[[x]])
+#       image.plot(Xhat)
+      if(length(pca.list) > 1) {
+            attr(pca.list, "variables") <- c(gridData$Variable$varName, "COMBINED")   
+      } else {
+            attr(pca.list, "variables") <- gridData$Variable$varName 
+      }       
+      attr(pca.list, "scaled:method") <- scaling
+      attr(pca.list, "xCoords") <- gridData$xyCoords$x
+      attr(pca.list, "yCoords") <- gridData$xyCoords$y
+      message("[", Sys.time(), "] Done")
+      return(pca.list)
+}
+# End      
+     
