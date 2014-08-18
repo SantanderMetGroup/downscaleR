77d0965c81ecb8ca4e2d1ecf1f4ccb2746406df4
jbedia <joaquin.bedia@gmail.com>
2014-08-18 19:11:46 +0200

    Documentation update

---
+++ man-roxygen/templateObsPredSim.R
@@ -0,0 +1,5 @@
+#' @param obs A field or station data containing the observations
+#' @param pred A field containing the prediction series 
+#' @param sim A field containing the simuation series
+
+
