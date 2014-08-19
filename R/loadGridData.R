88035ec2d132d0420147d3ecd653bb9bebc3216f
jbedia <joaquin.bedia@gmail.com>
2014-08-19 11:05:04 +0200

    Consistent dimension ordering

--- R/loadGridData.R
+++ R/loadGridData.R
@@ -45,41 +45,52 @@
 
 loadGridData <- function(dataset, var, dictionary = TRUE, lonLim = NULL,
                          latLim = NULL, season = NULL, years = NULL, time = "none") {
-    time <- match.arg(time, choices = c("none", "00", "06", "12", "18", "DD"))
-    aux.level <- findVerticalLevel(var)
-    var <- aux.level$var
-    level <- aux.level$level
-    # Dictionary lookup
-    if (dictionary == FALSE) {
-        dic <- NULL
-        shortName <- var
-    } else {
-        if (isTRUE(dictionary)) {
-            dicPath <- gsub("ncml$", "dic", dataset)
-        }
-        if (is.character(dictionary)) {
-            dicPath <- dictionary
-        }
-        dic <- dictionaryLookup(dicPath, var, time)
-        shortName <- dic$short_name          
-    }
-    if (is.null(season)) {
-        season <- 1:12
-    }
-    if (min(season) < 1 | max(season) > 12) {
-        stop("Invalid season definition")
-    }
-    gds <- J("ucar.nc2.dt.grid.GridDataset")$open(dataset)
-    grid <- gds$findGridByShortName(shortName)
-    if (is.null(grid)) {
-        stop("Variable '", shortName, "' not found\nCheck variable names using 'datasetInventory' and/or dictionary 'identifiers'")
-    }
-    latLon <- getLatLonDomain(grid, lonLim, latLim)
-    out <- loadGridDataset(var, grid, dic, level, season, years, time, latLon)
-    # Definition of projection
-    proj <- grid$getCoordinateSystem()$getProjection()$toString()
-    attr(out$xyCoords, which = "projection") <- proj
-    gds$close()
-    message("[",Sys.time(),"]", " Done")
-    return(out)
+      time <- match.arg(time, choices = c("none", "00", "06", "12", "18", "DD"))
+      aux.level <- findVerticalLevel(var)
+      var <- aux.level$var
+      level <- aux.level$level
+      # Dictionary lookup
+      if (dictionary == FALSE) {
+            dic <- NULL
+            shortName <- var
+      } else {
+            if (isTRUE(dictionary)) {
+                  dicPath <- gsub("ncml$", "dic", dataset)
+            }
+            if (is.character(dictionary)) {
+                  dicPath <- dictionary
+            }
+            dic <- dictionaryLookup(dicPath, var, time)
+            shortName <- dic$short_name          
+      }
+      if (is.null(season)) {
+            season <- 1:12
+      }
+      if (min(season) < 1 | max(season) > 12) {
+            stop("Invalid season definition")
+      }
+      gds <- J("ucar.nc2.dt.grid.GridDataset")$open(dataset)
+      grid <- gds$findGridByShortName(shortName)
+      if (is.null(grid)) {
+            stop("Variable '", shortName, "' not found\nCheck variable names using 'datasetInventory' and/or dictionary 'identifiers'")
+      }
+      latLon <- getLatLonDomain(grid, lonLim, latLim)
+      out <- loadGridDataset(var, grid, dic, level, season, years, time, latLon)
+      # Definition of projection
+      proj <- grid$getCoordinateSystem()$getProjection()$toString()
+      attr(out$xyCoords, which = "projection") <- proj
+      # Dimension ordering
+      tab <- c("time", "level", "lat", "lon")
+      x <- attr(out$Data, "dimensions")
+      if (length(x) > 1) {
+            b <- na.exclude(match(tab, x))
+            dimNames <- attr(out$Data, "dimensions")[b]
+            out$Data <- aperm(out$Data, perm = b)    
+            attr(out$Data, "dimensions")  <- dimNames
+      }
+      # Source Dataset and other metadata 
+      attr(out, "dataset") <- dataset
+      gds$close()
+      message("[",Sys.time(),"]", " Done")
+      return(out)
 }      
