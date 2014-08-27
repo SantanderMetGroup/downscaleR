f3dd2c8b701d34cfd526079c79da073ea9ffab48
jbedia <joaquin.bedia@gmail.com>
2014-08-27 17:42:36 +0200

    Bug fix in analogs
    Handles exception when one single field (either its PCs or not) is used as predictor

--- R/analogs.R
+++ R/analogs.R
@@ -114,6 +114,9 @@ analogs <- function(obs, pred, sim, n.neigh = 1, sel.fun = c("random", "mean")) 
             mu.list <- lapply(1:n.vars, function(x) {attributes(pred[[x]][[1]])$"scaled:center"})
             sigma.list <- lapply(1:n.vars, function(x) {attributes(pred[[x]][[1]])$"scaled:scale"})
             pred.mat <- pred$COMBINED[[1]]$PCs
+            if (is.null(pred.mat)) {
+                  pred.mat <- pred[[1]][[1]]$PCs
+            }      
       } else {
             # pred rescaled matrix
             dimNames <- attr(pred$Data, "dimensions")
@@ -140,6 +143,7 @@ analogs <- function(obs, pred, sim, n.neigh = 1, sel.fun = c("random", "mean")) 
                   attr(aux, "scaled:center") <- mu
                   attr(aux, "scaled:scale") <- sigma
                   Xsc.list <- list(aux)
+                  aux <- NULL
             }
             # Scaling parameters
             mu.list <- lapply(1:n.vars, function(x) attributes(Xsc.list[[x]])$"scaled:center")
@@ -186,33 +190,42 @@ analogs <- function(obs, pred, sim, n.neigh = 1, sel.fun = c("random", "mean")) 
                   })
             } else { # Field (no multimember)
                   aux <- array3Dto2Dmat(sim$Data)
-                  aux <- (aux - mu.list[[x]]) / sigma.list[[x]]
+                  aux <- (aux - mu.list[[1]]) / sigma.list[[1]]
                   simsc.list <- list(aux)
+                  aux <- NULL
             }
       }
       # Sim 2D matrix
       if (length(simsc.list) > 1) {
-            sim.mat <- rep(list(bquote()), n.mem)
-            for (i in 1:n.mem) {
-                  aux <- lapply(1:length(simsc.list), function(x) simsc.list[[x]][[i]])
-                  sim.mat[[i]] <- do.call("cbind", aux)
+            if ("member" %in% attr(sim$Data, "dimensions")) {
+                  sim.mat <- rep(list(bquote()), n.mem)
+                  for (i in 1:n.mem) {
+                        aux <- lapply(1:length(simsc.list), function(x) simsc.list[[x]][[i]])
+                        sim.mat[[i]] <- do.call("cbind", aux)
+                  }
+            } else {
+                  sim.mat <- list(do.call("cbind", simsc.list))
             }
-            simsc.list <- NULL
       } else {
             sim.mat <- simsc.list
-            simsc.list <- NULL
       }
+      simsc.list <- NULL
       # Projection of simulated field onto the predictor EOFs       
       if (isTRUE(use.PCs)) {
-            sim.mat <- lapply(1:length(sim.mat), function(x) t(t(pred$COMBINED[[1]]$EOFs) %*% t(sim.mat[[x]])))
+            sim.mat <- tryCatch(expr = lapply(1:length(sim.mat), function(x) {
+                        t(t(pred$COMBINED[[1]]$EOFs) %*% t(sim.mat[[x]]))
+                  }), error = function(err) {
+                        l <- lapply(1:length(sim.mat), function(x) {t(t(pred[[1]][[1]]$EOFs) %*% t(sim.mat[[x]]))})
+                        return(l)
+                  })
       }
       pred <- NULL
       # Analog search      
       message("[", Sys.time(), "] Calculating analogs ...")
       d.list <- lapply(1:length(sim.mat), function(x) {
             aux <- rdist(sim.mat[[x]], pred.mat)
-            b <- apply(aux, 1, function(vec, n.neigh) {sort(vec, index.return = TRUE)$ix[1:n.neigh]}, n.neigh)
-            return(b)
+            aux <- apply(aux, 1, function(vec, n.neigh) {sort(vec, index.return = TRUE)$ix[1:n.neigh]}, n.neigh)
+            return(aux)
       })
       pred.mat <- NULL
       # Analog assignation
