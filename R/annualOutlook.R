774713bf40647bbdc469eadf50eff9f5ba726607
miturbide <maibide@gmail.com>
2015-09-30 10:32:17 +0200

    Minor changes

--- R/annualOutlook.R
+++ R/annualOutlook.R
@@ -94,10 +94,10 @@ annualOutlook <- function(obs, pred, downscaled, location = c(-40, -5), yrange =
       ma <-  floor(max(c(x,y,w)))
       yrange <- c(mi, ma + (ma-mi))}
     
-    plot(1:length(period), x, xlim = c(0,length(period)), ylim = yrange, xlab = "Years", xaxt = "n", 
-         ylab = "Annual mean of the Daily var", cex = .6, col = NULL, main = "Annual series")
+    plot(1:length(period), x, xlim = c(0,length(period)), ylim = yrange, xlab="", xaxt = "n", 
+         ylab = "Annual/seasonal mean value", cex = .6, col = NULL)
     tck <- axis(1, at = 1:length(period), labels=FALSE)
-    text(tck,  par("usr")[3] - 2, xpd = TRUE, labels = (1981:2010), 
+    text(tck,  par("usr")[3] - 1, xpd = TRUE, labels = (1981:2010), 
          srt = 90, cex =.6)
     #plot the sd (shadows)
     polygon(x = c(1:length(period), length(period):1), y =c (ys+y,rev(y-ys)), col = rgb(1,0,0,0.2), border = NA)
@@ -115,13 +115,13 @@ annualOutlook <- function(obs, pred, downscaled, location = c(-40, -5), yrange =
     lines((length(train.period)+1):length(period), w, col = "blue", lwd = 2)
     
     legend(0, yrange[2] , legend = c("obs",  
-                                   paste("direct: rho=", 
-                                         round(rho.direct, digits = 2), ", bias=", 
-                                         round(bias.direct, digits = 2), sep = ""), 
-                                   paste("downscaled: rho=", 
-                                         round(rho.down, digits = 2), ", bias=", 
-                                         round(bias.down, digits = 2), sep = "")), 
-           fill = c("black", "red", "blue"), box.lwd = 0, cex = .6)
+                                     paste("direct: rho=", 
+                                           round(rho.direct, digits = 2), ", bias=", 
+                                           round(bias.direct, digits = 2), sep = ""), 
+                                     paste("downscaled: rho=", 
+                                           round(rho.down, digits = 2), ", bias=", 
+                                           round(bias.down, digits = 2), sep = "")), 
+           fill = c("black", "red", "blue"), box.lwd = 0, cex = .8)
     
     
     
@@ -141,9 +141,9 @@ annualOutlook <- function(obs, pred, downscaled, location = c(-40, -5), yrange =
     ### Spearman correlation rho and the Root Mean Square Error (RMSE) as accuracy measures 
     ### for the direct and calibrated simulation in the TEST PERIOD
     
-    xt <- tapply(obs.test$Data[, xo, yo], INDEX = test.id, FUN = mean)
+    xt <- x[(1+length(train.period)):length(period)]
     
-    yt <- tapply(pred.test$Data[,yi,xi], INDEX = test.id, FUN = mean)
+    yt <- y[(1+length(train.period)):length(period)]
     
     rmse.down <- sqrt(mean((xt - w)^2))
     bias.down <-  sum(w - xt)/sum(x)
@@ -158,15 +158,11 @@ annualOutlook <- function(obs, pred, downscaled, location = c(-40, -5), yrange =
       ma <-  floor(max(c(x,y,w)))
       yrange <- c(mi, ma + (ma-mi))}
     
-    plot(1:length(period), x, xlim = c(0,length(period)), ylim = yrange, xlab = "Years", xaxt = "n", 
-         ylab = "Annual mean of the Daily var", cex = .6, col = NULL, main = "Annual series")
+    plot(1:length(period), x, xlim = c(0,length(period)), ylim = yrange, xlab="", xaxt = "n", 
+         ylab = "Annual/seasonal mean value", cex = .6, col = NULL)
     tck <- axis(1, at = 1:length(period), labels=FALSE)
-    text(tck,  par("usr")[3] - 2, xpd = TRUE, labels = (1981:2010), 
+    text(tck,  par("usr")[3] - 1, xpd = TRUE, labels = (1981:2010), 
          srt = 90, cex =.6)
-    #plot the sd (shadows)
-    polygon(x = c(1:length(period), length(period):1), y =c (ys+y,rev(y-ys)), col = rgb(1,0,0,0.2), border = NA)
-    polygon(x = c((length(train.period)+1):length(period), length(period):(length(train.period)+1)), 
-            y =c (ws+w,rev(w-ws)), col = rgb(0,0,1,0.2), border = NA)
     #plot the mean (lines)
     lines(1:(length(train.period)+1), x[1:(length(train.period)+1)], lwd = 2, lty = 4, xlim = c(0,length(period)))
     lines((1+length(train.period)):length(period) , x[(1+length(train.period)):length(period)], 
@@ -179,13 +175,13 @@ annualOutlook <- function(obs, pred, downscaled, location = c(-40, -5), yrange =
     lines((length(train.period)+1):length(period), w, col = "blue", lwd = 2)
     
     legend(0, yrange[2] , legend = c("obs",  
-                                   paste("direct: rho=", 
-                                         round(rho.direct, digits = 2), ", bias=", 
-                                         round(bias.direct, digits = 2), sep = ""), 
-                                   paste("downscaled: rho=", 
-                                         round(rho.down, digits = 2), ", bias=", 
-                                         round(bias.down, digits = 2), sep = "")), 
-           fill = c("black", "red", "blue"), box.lwd = 0, cex = .6)
+                                     paste("direct: rho=", 
+                                           round(rho.direct, digits = 2), ", bias=", 
+                                           round(bias.direct, digits = 2), sep = ""), 
+                                     paste("downscaled: rho=", 
+                                           round(rho.down, digits = 2), ", bias=", 
+                                           round(bias.down, digits = 2), sep = "")), 
+           fill = c("black", "red", "blue"), box.lwd = 0, cex = .8)
     
     
   }
