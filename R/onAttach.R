98e07a93758d8f0454b0c1de10183a1bb515f024
jbedia <joaquin.bedia@gmail.com>
2016-05-13 13:46:12 +0200

    Minor changes in onAttach

--- R/onAttach.R
+++ R/onAttach.R
@@ -8,7 +8,7 @@
       builddate <- packageDescription(pkgname)$Date
       mess <- paste(pkgname, " version ", ver, " (", builddate,") is loaded", sep = "")
       packageStartupMessage(mess)
-      url <- "https://raw.githubusercontent.com/SantanderMetGroup/downscaleR/master/DESCRIPTION"
+      url <- paste0("https://raw.githubusercontent.com/SantanderMetGroup/", pkgname, "/master/DESCRIPTION")
       con <- tryCatch(getURL(url, ssl.verifypeer = FALSE), error = function(er) {
             er <- NULL
             return(er)
@@ -17,8 +17,9 @@
             b <- readLines(textConnection(con))
             latest.ver <- package_version(gsub("Version: ", "", b[grep("Version", b)]))
             if (ver < latest.ver) {
-                  ver.mess1 <- paste("WARNING: Your current version of ", pkgname, " (v", ver, ") is not up-to-date", sep = "")
-                  ver.mess <- paste("Get the latest stable version (", latest.ver, ") using <devtools::install_github('SantanderMetGroup/downscaleR'>)", sep = "")
+                  ver.mess1 <- paste0("WARNING: Your current version of ", pkgname, " (v", ver, ") is not up-to-date")
+                  ver.mess <- paste0("Get the latest stable version (", latest.ver,
+                                     ") using <devtools::install_github('SantanderMetGroup/", pkgname, "'>)")
                   packageStartupMessage(ver.mess1)
                   packageStartupMessage(ver.mess)
             }
