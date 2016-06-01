8dabd3f3389ca64d2a9582c8d5130c53f4194fb7
jbedia <joaquin.bedia@gmail.com>
2016-06-01 12:30:30 +0200

    Minor changes for better readibility

--- R/redim.R
+++ R/redim.R
@@ -5,50 +5,58 @@
 #' @param runtime logical. Add runtime dimension (default = FALSE)
 #' @param drop logical. Drop dimensions of length = 1 (default = FALSE)
 #' @return The same object with all the dimensions (i.e. member, time, station)
+#' @details The function down not handle multigrids (i.e. \code{"var"} dimension).
+#'  Thus, multigrids need to be subsetted along \code{"var"} prior to redimensioning.
 #' @keywords internal
 #' @importFrom abind abind
 #' @importFrom stats na.omit
-#' @author M. Iturbide
+#' @author M. Iturbide, J. Bedia
 
-redim <- function(obj, member = TRUE, runtime = FALSE, drop = FALSE) {
-      if (drop == FALSE) {
-            if ("station" %in% attr(obj$Data, "dimensions")) {
+redim <- function(obj,
+                  member = TRUE,
+                  runtime = FALSE,
+                  drop = FALSE) {
+      stopifnot(is.logical(member) | is.logical(runtime) | is.logical(drop))
+      dimNames <- getDim(obj)
+      if (!drop) {
+            if ("station" %in% dimNames) {
                   # Add fake 'coordinates dimension' dimension  -------
-                  ind <- which(attr(obj$Data, "dimensions") == "station")
-                  dimNames <- c(attr(obj$Data, "dimensions")[-ind], "lat", "lon")
+                  ind <- match("station", dimNames)
+                  dimNames <- c(dimNames[-ind], "lat", "lon")
                   obj$Data <- unname(abind(obj$Data, NULL, along = ind + 1))
                   attr(obj$Data, "dimensions") <- dimNames
             }
             # Add fake 'time' dimension  -------
-            if (!("time" %in% attr(obj$Data, "dimensions"))) {
-                  dimNames <- c("time", attr(obj$Data, "dimensions"))
+            if (!("time" %in% dimNames)) {
+                  dimNames <- c("time", dimNames)
                   obj$Data <- unname(abind(obj$Data, NULL, along = 0))
                   attr(obj$Data, "dimensions") <- dimNames
             }
             # Add fake 'member' dimension  -------
-            if (!("member" %in% attr(obj$Data, "dimensions")) & member == TRUE) {
-                  dimNames <- c("member", attr(obj$Data, "dimensions"))
+            if (!("member" %in% dimNames) & isTRUE(member)) {
+                  dimNames <- c("member", dimNames)
                   obj$Data <- unname(abind(obj$Data, NULL, along = 0))
                   attr(obj$Data, "dimensions") <- dimNames
             }
             # Add fake runtime dimension to deterministic/obs -----------
-            if (!("runtime" %in% attr(obj$Data, "dimensions")) & runtime == TRUE) {
-                  dimNames <- c("runtime", attr(obj$Data, "dimensions"))
+            if (!("runtime" %in% dimNames) & isTRUE(runtime)) {
+                  dimNames <- c("runtime", dimNames)
                   obj$Data <- unname(abind(obj$Data, NULL, along = -1))    
                   attr(obj$Data, "dimensions") <- dimNames
             }
-            dimNames <- c("runtime", "member","time","lat","lon")
+            dimNames <- c("runtime", "member", "time", "lat", "lon")
             dimNames.aux <- attr(obj$Data, "dimensions")
             perm <- na.omit(match(dimNames, dimNames.aux))
             obj$Data <- aperm(obj$Data, perm)
             obj[["Data"]] <- unname(obj$Data)
             attr(obj[["Data"]], "dimensions") <- dimNames.aux[perm]
       } else {
-            if (1 %in% dim(obj$Data)) {
-                  dimNames <- attr(obj$Data, "dimensions")[-which(dim(obj$Data) == 1)]
+            shp <- dim(obj[["Data"]])
+            if (1 %in% shp) {
+                  dimNames <- dimNames[-match(1, shp)]
                   obj$Data <- drop(obj$Data)
                   attr(obj$Data, "dimensions") <- dimNames
-                  if ("lat" %in% dimNames & "lon" %in% dimNames == FALSE) {
+                  if (!("lat" %in% dimNames && "lon" %in% dimNames)) {
                         attr(obj$Data, "dimensions")[attr(obj$Data, "dimensions") == "lat"] <- "station"
                   }
             }
