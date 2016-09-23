0360fb143c1431435c5e3d5f2c93ac5af01757ea
jbedia <joaquin.bedia@gmail.com>
2016-09-23 11:14:13 +0200

    Update startup info

--- R/onAttach.R
+++ R/onAttach.R
@@ -13,13 +13,14 @@
             er <- NULL
             return(er)
       })
+      packageStartupMessage("###################################################################\nIMPORTANT NOTE: Since downscaleR v2.0-0, the new dependency package\ntransformeR is introduced. Data manipulation functions are now part\nof transformeR: <https://github.com/SantanderMetGroup/transformeR>\n###################################################################")
       if (!is.null(con)) {
             b <- readLines(textConnection(con))
             latest.ver <- package_version(gsub("Version: ", "", b[grep("Version", b)]))
             if (ver < latest.ver) {
                   ver.mess1 <- paste0("WARNING: Your current version of ", pkgname, " (v", ver, ") is not up-to-date")
                   ver.mess <- paste0("Get the latest stable version (", latest.ver,
-                                     ") using <devtools::install_github('SantanderMetGroup/", pkgname, "')>")
+                                     ") using <devtools::install_github(c('SantanderMetGroup/transformeR','SantanderMetGroup/", pkgname, "'))>")
                   packageStartupMessage(ver.mess1)
                   packageStartupMessage(ver.mess)
             }
