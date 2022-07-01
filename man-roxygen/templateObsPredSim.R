02ec49d181287ec1eb4a979190473a84322a7405
removedporn <86824510+removedporn@users.noreply.github.com>
2022-07-01 12:29:40 +0800

    Delete man-roxygen directory

--- man-roxygen/templateObsPredSim.R
+++
@@ -1,9 +0,0 @@
-#' @param y A grid or station data containing the observed climate data for the training period
-#' @param x A grid containing the simulated climate by the model for the training period. This can be either
-#'  the same variable as \code{obs}, in the case of model calibration (bias correction and related techniques) or MOS (model
-#'   output statistics) approaches, or a set of predictors in the case of \emph{perfect prog} downscaling approaches (possibly
-#'   after principal component analysis via the \code{\link{prinComp}} output).
-#' @param newdata A grid containing the simulated climate for the variables used in \code{x}, but considering the test period.
-#' If \code{x} is a multimember grid (the member dimension is > 1), the function assumes that the order of 
-#' the members is the same in \code{newdata}.
-
--- man-roxygen/templateParallelParams.R
+++
@@ -1,4 +0,0 @@
-#' @param parallel Logical. Should parallel execution be used?
-#' @param max.ncores Integer. Upper bound for user-defined number of cores.
-#' @param ncores Integer number of cores used in parallel computation. Self-selected number of
-#'  cores is used when \code{ncpus = NULL} (the default), or when \code{maxcores} exceeds the default \code{ncores} value.
